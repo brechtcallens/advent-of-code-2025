579bb8e7862887664f62c39f717de88c363c8d8a
Brecht Callens <brecht.callens@ericsson.com>
2025-12-01 09:58:02 +0100

    Solution for day 1

--- src/Solutions/Day01/Day01Solver.cs
+++ src/Solutions/Day01/Day01Solver.cs
@@ -6,11 +6,73 @@ public class Day01Solver : BaseDaySolver
 
     protected override string SolvePart1(string[] input)
     {
-        throw new NotImplementedException("Missing implementation for part 1.");
+        var solution = 0;
+
+        var position = 50;
+        foreach (var line in input)
+        {
+            var direction = line[0];
+            var change = int.Parse(line[1..]);
+
+            switch (direction)
+            {
+                case 'L':
+                    position -= change;
+                    break;
+                case 'R':
+                    position += change;
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unknown direction: {direction}");
+            }
+
+            position = (position + 100) % 100;
+
+            if (position == 0)
+            {
+                solution++;
+            }
+        }
+        return $"{solution}";
     }
 
     protected override string SolvePart2(string[] input)
     {
-        throw new NotImplementedException("Missing implementation for part 2.");
+        var solution = 0;
+
+        var position = 50;
+        foreach (var line in input)
+        {
+            var prevPosition = position;
+            var direction = line[0];
+            var change = int.Parse(line[1..]);
+            
+            var times = change / 100;
+            change %= 100;
+
+            switch (direction)
+            {
+                case 'L':
+                    position -= change;
+                    break;
+                case 'R':
+                    position += change;
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unknown direction: {direction}");
+            }
+
+            solution += times;
+            if (position <= 0 || position >= 100)
+            {
+                position = (position + 100) % 100;
+                if (prevPosition != 0)
+                {
+                    solution ++;
+                }
+            }
+        }
+        
+        return $"{solution}";
     }
 }
