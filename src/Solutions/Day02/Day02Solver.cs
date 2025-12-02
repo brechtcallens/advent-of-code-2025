6351712e98000f6686dd8a98c5b9d1c2919bfbd2
Brecht Callens <brecht.callens@outlook.com>
2025-12-02 08:08:17 +0100

    Remove duplication

--- src/Solutions/Day02/Day02Solver.cs
+++ src/Solutions/Day02/Day02Solver.cs
@@ -6,7 +6,13 @@ public class Day02Solver : BaseDaySolver
 {
     public override int Day => 2;
 
-    protected override string SolvePart1(string[] input)
+    protected override string SolvePart1(string[] input) =>
+         SolveGeneric(input, @"^(\d+)\1$");
+
+    protected override string SolvePart2(string[] input) =>
+        SolveGeneric(input, @"^(\d+)\1+$");
+
+    private string SolveGeneric(string[] input, string regexString)
     {
         long invalidIds = 0;
         var ranges = input[0].Split(',')
@@ -23,35 +29,7 @@ public class Day02Solver : BaseDaySolver
 
             for (long n = startNumber; n <= endNumber; n++)
             {
-                var conditionRegex = new Regex(@"^(\d+)\1$");
-                var check = conditionRegex.Match(n.ToString());
-                if (check.Success)
-                {
-                    invalidIds += n;
-                }
-            }
-        }
-        return invalidIds.ToString();
-    }
-
-    protected override string SolvePart2(string[] input)
-    {
-        long invalidIds = 0;
-        var ranges = input[0].Split(',')
-            .ToList()
-            .Select(range => range.Split("-").ToList());
-        
-        foreach (var range in ranges)
-        {
-            var start = range[0];
-            var end = range[1];
-
-            var startNumber = long.Parse(start);
-            var endNumber = long.Parse(end);
-
-            for (long n = startNumber; n <= endNumber; n++)
-            {
-                var conditionRegex = new Regex(@"^(\d+)\1+$");
+                var conditionRegex = new Regex(regexString);
                 var check = conditionRegex.Match(n.ToString());
                 if (check.Success)
                 {
