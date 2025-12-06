811faaf38e73400934af8a5b47747b253a90ef44
Brecht Callens <brecht.callens@outlook.com>
2025-12-06 10:38:27 +0100

    Redid day 6 as if it was written in Typescript

--- src/Solutions/Day06/Day06Solver.cs
+++ src/Solutions/Day06/Day06Solver.cs
@@ -6,99 +6,71 @@ public class Day06Solver : BaseDaySolver
 {
     public override int Day => 6;
 
+    private static readonly string digitMatchRegexString = @"\d+";
+    
+    private readonly Regex digitGroupsRegex = new(digitMatchRegexString);
+
     protected override string SolvePart1(string[] input)
     {
-        var numbersLines = input[..^1].Select(line =>
-            Regex.Split(line, @"\s+")
-                .Where(s => !string.IsNullOrEmpty(s))
-                .Select(s => long.Parse(s.Trim()))
-                .ToArray())
+        var numbersGroupedByLine = input[..^1]
+            .Select(line => 
+                digitGroupsRegex
+                    .Matches(line)
+                    .Select((match) => long.Parse(match.Value))
+                    .ToArray())
             .ToArray();
         
-        var operationLine = Regex.Split(input[^1], @"\s+")
-            .Where(s => !string.IsNullOrEmpty(s))
-            .Select(s => s.Trim())
+        var operations = input[^1]
+            .Where(c => c != ' ')
             .ToArray();
 
-        var grandTotal = 0L;
-        for (int exercise = 0; exercise < operationLine.Length; exercise++)
-        {
-            var op = operationLine[exercise];
-            switch (op)
-            {
-                case "+":
-                    {
-                        grandTotal += numbersLines
-                            .Select(line => line[exercise])
-                            .Sum();
-                        break;
-                    }
-                case "*":
-                    {
-                        grandTotal += numbersLines
-                            .Select(line => line[exercise])
-                            .Aggregate(1L, (acc, val) => acc * val);
-                        break;
-                    }
-                default:
-                    throw new InvalidOperationException($"Unknown operation '{op}' for exercise {exercise}.");
-            }
-        }
+        var grandTotal = operations
+            .Select((operation, exercise) =>
+                calculateExercise(operation, numbersGroupedByLine.Select((numbers) => numbers[exercise])))
+            .Sum();
+
         return grandTotal.ToString();
     }
 
     protected override string SolvePart2(string[] input)
     {
-        var operationLine = Regex.Split(input[^1], @"\s+")
-            .Where(s => !string.IsNullOrEmpty(s))
-            .Select(s => s.Trim())
+        var operations = input[^1]
+            .Where(c => c != ' ')
             .ToArray();
-        
-        var grid = input[..^1]
+
+        var numberGrid = input[..^1]
             .Select(line => line.ToCharArray())
             .ToArray();
-        
-        var numbersLists = new List<List<long>>();
-        var numbers = new List<long>();
-        for (int j = 0; j < grid[0].Length; j++)
-        {
-            var x = new string(grid
-                .Select(line => line[j])
-                .Where(character => character != ' ')
-                .ToArray());
-            
-            if (x.Length != 0)
-            {
-                numbers.Add(long.Parse(x));
-            }
-            else
-            {
-                numbersLists.Add(numbers);
-                numbers = new List<long>();
-            }
-        }
-        numbersLists.Add(numbers);
 
-        var grandTotal = 0L;
-        for (int exercise = 0; exercise < operationLine.Length; exercise++)
-        {
-            var op = operationLine[exercise];
-            switch (op)
-            {
-                case "+":
-                    {
-                        grandTotal += numbersLists[exercise].Sum();
-                        break;
-                    }
-                case "*":
-                    {
-                        grandTotal += numbersLists[exercise].Aggregate(1L, (acc, val) => acc * val);
-                        break;
-                    }
-                default:
-                    throw new InvalidOperationException($"Unknown operation '{op}' for exercise {exercise}.");
-            }
-        }
+        var numbersGroupedByExercise = numberGrid[0]
+            .Select((_, column) =>
+                numberGrid.Select(line => line[column])
+                    .Where(character => character != ' ')
+                    .ToArray())
+            .Aggregate(new List<List<long>> { new() },
+                (acc, value) =>
+                {
+                    if (value.Length == 0)
+                        acc.Add([]);
+                    else
+                        acc[^1].Add(long.Parse(new string(value)));
+                    return acc;
+                });
+
+        var grandTotal = operations
+            .Select((operation, exercise) =>
+                calculateExercise(operation, numbersGroupedByExercise[exercise]))
+            .Sum();
+
         return grandTotal.ToString();
     }
+
+    private static long calculateExercise(char operation, IEnumerable<long> numbers) =>
+         operation switch
+         {
+             '+' => numbers.Sum(),
+             '*' => numbers.Aggregate(1L, (acc, val) => acc * val),
+             _ => throw new InvalidOperationException($"Unknown operation '{operation}'."),
+         };
+
 }
