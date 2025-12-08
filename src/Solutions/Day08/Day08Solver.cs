a233a2618320734fc1ca3720683bd057c4e82eca
Brecht Callens <brecht.callens@outlook.com>
2025-12-08 17:15:33 +0100

    Cleanup of day 8 and self-calculated max of three

--- src/Solutions/Day08/Day08Solver.cs
+++ src/Solutions/Day08/Day08Solver.cs
@@ -1,7 +1,4 @@
-using System.Dynamic;
-using System.Formats.Asn1;
-using System.Linq.Expressions;
-using System.Security.AccessControl;
+using System.Data;
 
 namespace AdventOfCode2025.Solutions.Day08;
 
@@ -14,13 +11,13 @@ public class Day08Solver : BaseDaySolver
         var boxes = GetBoxesFromInput(input);
         var boxDistances = GetConnectionDistancesFromBoxes(boxes);
 
-        var workingSets = new Stack<HashSet<int>>();
-        foreach (var (_, connection) in boxDistances[..1000])
-        {
-            workingSets.Push([connection.l, connection.r]);
-        }
+        var workingSets = new Stack<HashSet<int>>(
+            boxDistances[..boxes.Length]
+                .Select(boxDistance => boxDistance.connection)
+                .Select(connection => new HashSet<int>([connection.l, connection.r]))
+        );
 
-        var finalSetSizes = new List<int>();
+        int[] largestCircuits = [-1, -1, -1];
         while (workingSets.TryPop(out var currentSet))
         {
             bool mergedWithSomething = false;
@@ -35,14 +32,15 @@ public class Day08Solver : BaseDaySolver
             }
             if (!mergedWithSomething)
             {
-                finalSetSizes.Add(currentSet.Count);
+                var minIndex = largestCircuits[0] <= largestCircuits[1] && largestCircuits[0] <= largestCircuits[2]
+                        ? 0
+                        : largestCircuits[1] <= largestCircuits[2]
+                            ? 1
+                            : 2;
+                largestCircuits[minIndex] = Math.Max(currentSet.Count, largestCircuits[minIndex]);
             }
         }
-        finalSetSizes.Sort();
-
-        return finalSetSizes[^3..]
-            .Aggregate((acc, val) => acc * val)
-            .ToString();
+        return largestCircuits.Aggregate((acc, val) => acc * val).ToString();
     }
 
     protected override string SolvePart2(string[] input)
@@ -72,7 +70,7 @@ public class Day08Solver : BaseDaySolver
                         // stop and return the answer.
                         if (otherWorkingSet.Count == boxes.Length)
                         {
-                            return (boxes[connection.l][0] * boxes[connection.r][0]).ToString();
+                            return (boxes[connection.l].x * boxes[connection.r].x).ToString();
                         }
                         break;
                     }
@@ -96,7 +94,7 @@ public class Day08Solver : BaseDaySolver
         return "invalid";
     }
 
-    private static List<(double distance, (int l, int r) connection)> GetConnectionDistancesFromBoxes(long[][] boxes)
+    private static List<(double distance, (int l, int r) connection)> GetConnectionDistancesFromBoxes((long x, long y, long z)[] boxes)
     {
         var connectionDistances = new List<(double distance, (int l, int r) connection)>(boxes.Length * boxes.Length / 2);
         for (int i = 0; i < boxes.Length; i++)
@@ -104,9 +102,9 @@ public class Day08Solver : BaseDaySolver
             for (int j = i + 1; j < boxes.Length; j++)
             {
                 var distance = Math.Sqrt(
-                    Math.Pow(boxes[i][0] - boxes[j][0], 2)
-                    + Math.Pow(boxes[i][1] - boxes[j][1], 2)
-                    + Math.Pow(boxes[i][2] - boxes[j][2], 2));
+                    Math.Pow(boxes[i].x - boxes[j].x, 2)
+                    + Math.Pow(boxes[i].y - boxes[j].y, 2)
+                    + Math.Pow(boxes[i].z - boxes[j].z, 2));
                 connectionDistances.Add((distance, (i, j)));
             }
         }
@@ -114,9 +112,10 @@ public class Day08Solver : BaseDaySolver
         return connectionDistances;
     }
 
-    private static long[][] GetBoxesFromInput(string[] input) =>
+    private static (long x, long y, long z)[] GetBoxesFromInput(string[] input) =>
         [.. input.Select(line =>
             line.Split(",")
                 .Select(long.Parse)
-                .ToArray())];
+                .ToArray())
+            .Select(line => (line[0], line[1], line[2]))];
 }
